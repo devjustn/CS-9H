c90e9f49df5c3e4bf7b841e5347b8a36efa9d017
Justin Lee <justin@Justins-MacBook-Pro.local>
2018-02-11 07:51:17 -0800

    initial commit

---
+++ Project-1/project1.py
@@ -0,0 +1,51 @@
+'''
+
+Cellular Automation, an array of cells that turn on/off based on whether other cells are on/off
+Program will print out an image in PBF
+Takes in two parameters: rule number, number of steps 
+
+'''
+
+import sys
+
+rule = int(sys.argv[1])
+steps = int(sys.argv[2]) #row
+
+# Set the rule
+
+
+# Init row
+
+#
+
+# Convert to binary
+# def convert_to_binary(i):
+# 	if i == 0:
+# 		# for x in range(0, c):
+# 		print(0)
+# 	else:
+# 		print((i >> 1) % 2, end = "")
+		
+# 		convert_to_binary(i >> 1)
+
+# Convert to binary
+def convert_to_binary(i):
+	binary_string = ""
+	while i >= 0:
+
+
+# Format binary to 8 bits
+def format_bin():
+	print("")
+
+def time():
+	print("nothing")
+
+def display():
+	print("P1", RULE_NUMBER, STEPS, sep = " ")
+	
+
+#def main():
+# print(30 >> 1)
+
+print(convert_to_binary(RULE_NUMBER))
