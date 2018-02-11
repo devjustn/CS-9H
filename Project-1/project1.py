e1ac52b1e6136cb0caa32c824ba2ee49b64863c1
devjustn <devjustn@gmail.com>
2018-02-11 13:52:32 -0800

    init rows complete, set_rule needs work, then pass values to all_rows

--- Project-1/project1.py
+++ Project-1/project1.py
@@ -10,42 +10,55 @@ import sys
 
 rule = int(sys.argv[1])
 steps = int(sys.argv[2]) #row
+BITS = 8
 
 # Set the rule
+def set_rule(n):
+	binary_number_array = []
+	bin_position_values = []
+	# Conversion
+	binary_number=bin(int(n))[2:].zfill(BITS)
+	# binary_number_array = []
+	for count0 in binary_number:
+		binary_number_array.append(count0)
+	
+	# count0 = 0
+	# count1 = 0
+	# for count0 in range(len(str(binary_number))):
+	# 	binary_number_array.append(count0)
+	for count1 in range(BITS):
+		# bin_values.append(int(bin(7-count1)[2:]).zfill(3))
+		bin_position_values.append(bin(int(7 - count1) )[2:].zfill(3)) # List of string containing 3 digit
+	print(binary_number_array)
+	print(bin_position_values)
+	print(dict(zip(binary_number_array, bin_position_values))) #failed
+	
+	print('')
+	print('')
 
-
+'''
+Need to share value of first, outer, last, position on first row
+'''
 # Init row
+def initial_row(row_length):
+	print("P1", rule, steps, sep = ' ') 
+	middle = (rule - 1)/2
+	current_string = ""
+	for i in range(rule):
+		if (i == middle):
+			current_string += "1"
+		else:
+			current_string += "0"
+	print(current_string)
+	return current_string
 
-#
-
-# Convert to binary
-# def convert_to_binary(i):
-# 	if i == 0:
-# 		# for x in range(0, c):
-# 		print(0)
-# 	else:
-# 		print((i >> 1) % 2, end = "")
-		
-# 		convert_to_binary(i >> 1)
-
-# Convert to binary
-def convert_to_binary(i):
-	binary_string = ""
-	while i >= 0:
-
-
-# Format binary to 8 bits
-def format_bin():
+def all_rows(rule, rows):
 	print("")
 
-def time():
-	print("nothing")
 
-def display():
-	print("P1", RULE_NUMBER, STEPS, sep = " ")
-	
+def main():
+	initial_row(rule)
 
-#def main():
-# print(30 >> 1)
+main()
 
-print(convert_to_binary(RULE_NUMBER))
+
