e10a776dbe488907ab9420af821b374c70cc3ccf
devjustn <devjustn@gmail.com>
2018-02-14 00:10:57 -0800

    assignment#2

---
+++ Project-2/project2.py
@@ -0,0 +1,21 @@
+'''
+Unit conversion
+
+Distances: ft cm mm mi m yd km in
+Weights: lb mg kg oz g
+Volumes: floz qt cup mL L gal pint
+
+'''
+
+def welcome_message():
+	print('Welcome to our Python-powered Unit Converter v1.0 by Dan Garcia! You can convert Distances , Weights , Volumes & Times to one another, but only \
+continue within units of the same category, which are shown below. E.g.: 1 mi in ft \
+')
+
+def bye_message():
+	print('Thanks for converting with us. Y'all come back now, y'hear?')
+
+def main():
+	welcome_message()
+
+main()
